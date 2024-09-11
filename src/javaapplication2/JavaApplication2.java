8452915e250bbcf3657c563869a729ed87af0fa9
AC-Lewis <lewis34@sheridancollege.ca>
2024-09-11 14:24:03 -0400

    Second commit: added?

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -15,7 +15,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) 
     {
-        System.out.println("Hello!");    // TODO code application logic here
+        System.out.println("Hello!?");    // TODO code application logic here
     }
     
 }
